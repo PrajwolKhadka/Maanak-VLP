15c26262aec13132e19224752cfbc260ddfff1a9
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-06-16 12:32:56 +0545

    feat(backend setup with admin): admin account seed

---
+++ backend/server.ts
@@ -0,0 +1,60 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import session from 'express-session';
+import passport from 'passport';
+import dotenv from 'dotenv';
+dotenv.config();
+import authRoutes from './routes/authRoutes';
+import topicRoutes from './routes/topicRoutes';
+import chapterRoutes from './routes/chapterRoutes';
+import lessonRoutes from './routes/lessonRoutes';
+import quizRoutes from './routes/quizRoutes';
+import noteRoutes from './routes/noteRoutes';
+import dashboardRoutes from './routes/dashboardRoutes';
+
+import connectDB from './config/db';
+
+import './config/passport';
+
+
+
+const app: Application = express();
+
+connectDB();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.JWT_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/topics', topicRoutes);
+app.use('/api/chapters', chapterRoutes);
+app.use('/api/lessons', lessonRoutes);
+app.use('/api/quiz', quizRoutes);
+app.use('/api/notes', noteRoutes);
+app.use('/api/dashboard', dashboardRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
