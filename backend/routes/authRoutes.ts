6ab962ee2fe3d358b16e660763453259163084dc
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-06-23 13:51:08 +0545

    refact(forgot-password):added forgot password

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import passport from 'passport';
-import { register, login, getMe, adminLogin, googleCallback } from '../controllers/authController';
+import { register, login, getMe, adminLogin, googleCallback, forgotPassword, resetPassword } from '../controllers/authController';
 import { protectUser } from '../middleware/authMiddleware';
 
 const router = express.Router();
@@ -9,7 +9,8 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protectUser, getMe);
 router.post('/admin/login', adminLogin);
-
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 // Google OAuth
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 router.get('/google/callback',
