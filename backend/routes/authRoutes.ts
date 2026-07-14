6517fc2695f8669e0a7300ee2f1ebef6300344bf
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-07-14 16:06:31 +0545

    delete account routes

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import passport from 'passport';
-import { register, login, getMe, adminLogin, googleCallback, forgotPassword, resetPassword } from '../controllers/authController';
+import { register, login, getMe, adminLogin, googleCallback, forgotPassword, resetPassword, updateAvatar, deleteAccount } from '../controllers/authController';
 import { protectUser } from '../middleware/authMiddleware';
+import { upload } from '../middleware/upload';
 
 const router = express.Router();
 
@@ -17,5 +18,7 @@ router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/login', session: false }),
   googleCallback
 );
+router.put('/avatar', protectUser, upload.single('avatar'),updateAvatar);
+router.put('/account', protectUser, deleteAccount);
 
 export default router;
